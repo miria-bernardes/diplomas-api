f19872cc2f68dc669cbed9dc2f9c1cc95897cc52
mbernardes <miria.bernardes@mercadolivre.com>
2021-07-14 17:23:07 -0300

    refatorando trechos do codigo para serem reutilizados

--- src/main/java/br/com/bootcamp/apidiplomas/services/DegreeService.java
+++ src/main/java/br/com/bootcamp/apidiplomas/services/DegreeService.java
@@ -1,21 +1,36 @@
 package br.com.bootcamp.apidiplomas.services;
 
 import br.com.bootcamp.apidiplomas.dto.DegreeDTO;
+import br.com.bootcamp.apidiplomas.dto.StudentAnalyzedDTO;
 import br.com.bootcamp.apidiplomas.dto.StudentDTO;
 import br.com.bootcamp.apidiplomas.dto.SubjectDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
 public class DegreeService {
 
     public DegreeDTO getDegree(StudentDTO student) {
-        double average = student.getSubjects().stream().mapToDouble(SubjectDTO::getNote).average().getAsDouble();
+        double average = calcAverage(student.getSubjects());
 
-        String message = average > 9 ? "Parabéns, " + student.getName() +
+        String message = withHonors(average) ? "Parabéns, " + student.getName() +
                 " você foi aprovado com uma das melhores notas da turma!" : "Diploma de " +
                 student.getName() + ".";
 
         return new DegreeDTO(message, average, student);
+    }
+
+    private boolean withHonors(double average) {
+        return average > 9;
+    }
+
+    public double calcAverage(List<SubjectDTO> subjects) {
+        return subjects.stream().mapToDouble(SubjectDTO::getNote).average().getAsDouble();
+    }
+
+    public StudentAnalyzedDTO analyzeNotes(StudentDTO student) {
+        return new StudentAnalyzedDTO(calcAverage(student.getSubjects()), student);
     }
 }
